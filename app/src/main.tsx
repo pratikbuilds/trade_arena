02e7083e824afdbf98d21209de49bf14e93827e7
0xPratik <sariapratik@gmail.com>
2026-04-19 15:23:16 +0545

    app: simplify scaffold to single-page shell

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,9 +1,6 @@
 import { StrictMode } from "react";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { RouterProvider } from "@tanstack/react-router";
 import { createRoot } from "react-dom/client";
-import { queryClient } from "@/lib/query-client";
-import { router } from "@/router";
+import App from "@/App";
 import "./index.css";
 
 const container = document.getElementById("root");
@@ -16,8 +13,6 @@ const root = createRoot(container);
 
 root.render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
 );
